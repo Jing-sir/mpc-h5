b13c48da1f79403e07cb5e5d0d860285af10ca7a
xiangnan <658551j@gmail.com>
2023-11-10 22:54:58 +0800

    feat: header tab样式调整

--- src/Main.vue
+++ src/Main.vue
@@ -4,12 +4,22 @@
       <header class="is-flex justify-content-space-between align-items-center home-header">
         <div>logo</div>
         <div class="is-flex flex-direction-row align-items-center header-right">
-          <van-tabs v-model:active="activeKey">
-            <van-tab title="トップページ" />
-            <van-tab title="速報" />
-            <van-tab title="ビジネス" />
-            <van-tab title="特集" />
-          </van-tabs>
+          <div class="tab-wrap">
+            <van-tabs v-model:active="activeKey">
+              <van-tab title="トップページ" />
+              <!--   首页   -->
+              <van-tab title="速報" />
+              <!--   快讯   -->
+              <van-tab title="ビジネス" />
+              <!--   商业   -->
+              <van-tab title="特集" />
+              <!--   专题   -->
+              <van-tab title="学习" />
+              <!--   学习   -->
+              <van-tab title="活动" />
+              <!--   活动   -->
+            </van-tabs>
+          </div>
           <van-icon class="search-icon" name="search" />
           <van-icon class="menu-icon" name="wap-nav" />
         </div>
@@ -41,7 +51,12 @@ const activeKey = ref('');
   padding: 0 12px;
   box-sizing: border-box;
 
-  .header-right {}
+  .header-right {
+  }
+
+  .tab-wrap {
+    width: 70vw;
+  }
 
   .search-icon, .menu-icon {
     margin-left: 4px;
